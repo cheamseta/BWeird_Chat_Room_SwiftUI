7ce4f7d0ddf81fcd3fa5391c4b8d1823c4dd5f6b
cheamseta <cheamseta@gmail.com>
2021-04-29 20:07:50 +0700

    Initial Commit

---
+++ BWeirdChatRoom/BWeirdChatRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BWeirdChatRoomApp.swift
+//  BWeirdChatRoom
+//
+//  Created by seta cheam on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BWeirdChatRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BWeirdChatRoom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BWeirdChatRoom
+//
+//  Created by seta cheam on 4/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
